249501ef06d9d33668eae76e2e87c119158f7890
1967 Chevrolet Camaro SS <61229333+camaro-67@users.noreply.github.com>
2021-02-23 09:22:12 +0700

    Update var_print.php

--- var_print.php
+++ var_print.php
@@ -8,3 +8,10 @@ function foo(){
 for($a=1;$a<=5;$a++){
     echo foo()."\n";
 }
+
+//bar
+//bar
+//bar
+//bar
+//bar
+//The require_once expression is identical to require except PHP will check if the file has already been included, and if so, not include (require) it again.
